4bddac574e15a9a1ac89a24d43030824e84111bd
Cereal <eyalrog1@gmail.com>
2023-07-25 17:35:34 +0300

    Added disabling the input textbox after the correct password is written, and type=text to the textbox.

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let OPENING_PASSWORD = "Lorem Ipsum";
 function checkOpeningPassword() {
     let passwordInput = document.getElementById("opening_password_input");
     if (passwordInput.value.toLowerCase() === OPENING_PASSWORD.toLowerCase()) {
+        passwordInput.setAttribute("disabled", "");
         open("book.html", "_self");
     }
 }
