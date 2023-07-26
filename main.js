95f2459467e6a2a7bfe84dcb66c66acc7f8fb096
Cereal <eyalrog1@gmail.com>
2023-07-26 14:20:56 +0300

    Added some flare to the opening page. Still need to finish scythe animation.

--- main.js
+++ main.js
@@ -2,8 +2,27 @@ let OPENING_PASSWORD = "Lorem Ipsum";
 
 function checkOpeningPassword() {
     let passwordInput = document.getElementById("opening_password_input");
+    let scythe_image = document.getElementById("scythe");
+
+    // Check if inputted password matches.
     if (passwordInput.value.toLowerCase() === OPENING_PASSWORD.toLowerCase()) {
         passwordInput.setAttribute("disabled", "");
-        open("book.html", "_self");
+        passwordInput.style.visibility = "hidden";
+        document.getElementById("riddle").style.visibility = "hidden";
+
+        // Show scythe.
+        scythe_image.style.visibility = "visible";
+
+        // Rotate scythe.
+        for (let rotation = 0; rotation < 360; rotation++) {
+            setTimeout(() => {
+                scythe_image.style.transform = `rotate(${-rotation}deg)`
+            }, rotation * 6);
+        }
+
+        setTimeout(() => {
+            open("book.html", "_self");
+        }, 2500);
+
     }
 }
